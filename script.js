b4702b4dc932e1ca722fb52a84b08ad39bcf43de
MUHAMMADaatirCOADER <muhammadaatir786786@gmail.com>
2026-07-14 16:36:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function red(){
+    document.getElementById("red").style.backgroundColor="red";
+    document.getElementById("green").style.backgroundColor="white";
+    document.getElementById("yellow").style.backgroundColor="white";
+}
+function green(){
+    document.getElementById("green").style.backgroundColor="green";
+    document.getElementById("red").style.backgroundColor="white";
+    document.getElementById("yellow").style.backgroundColor="white";
+}
+function yellow(){
+    document.getElementById("yellow").style.backgroundColor="yellow";
+     document.getElementById("green").style.backgroundColor="white";
+      document.getElementById("red").style.backgroundColor="white";
+
+}
