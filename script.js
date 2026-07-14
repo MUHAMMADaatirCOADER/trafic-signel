b03437cf4d19b69a07c2dfccfd9ac3045083278c
MUHAMMADaatirCOADER <muhammadaatir786786@gmail.com>
2026-07-14 13:10:37 -0700

    Enhance red, green, and yellow functions with car logic
    Updated traffic light functions to include car movement and stop display.

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+let car=document.getElementById("car");
 function red(){
     document.getElementById("red").style.backgroundColor="red";
     document.getElementById("green").style.backgroundColor="white";
     document.getElementById("yellow").style.backgroundColor="white";
+    car.style.transform="translateX(0px)";
+    document.getElementById("stop").style.display="inline-block";
 }
 function green(){
     document.getElementById("green").style.backgroundColor="green";
     document.getElementById("red").style.backgroundColor="white";
     document.getElementById("yellow").style.backgroundColor="white";
+    car.style.transform="translateX(800px)";
+    document.getElementById("stop").style.display="none";
 }
 function yellow(){
     document.getElementById("yellow").style.backgroundColor="yellow";
      document.getElementById("green").style.backgroundColor="white";
       document.getElementById("red").style.backgroundColor="white";
-
+      car.style.transform="translateX(0px)";
+      document.getElementById("stop").style.display="none";
 }
+
